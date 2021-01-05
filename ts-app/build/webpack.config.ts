9e2bee3a0f954b3695c4b07a678324cd914137da
ckl <kanglie.chen@jumho.cn>
2021-01-06 00:07:59 +0800

    更新代码

--- ts-app/build/webpack.config.ts
+++ ts-app/build/webpack.config.ts
@@ -1,32 +1,35 @@
 import * as path from 'path';
 import webpack from 'webpack';
+import * as npmPackage from '../package.json';
 import * as ESLintPlugin from 'eslint-webpack-plugin';
 import * as nodeExternals from 'webpack-node-externals';
 
 const rootDir = path.resolve(__dirname, '../');
+
+// alias
+const alias: {[tag: string]: string} = {};
+for (const item of Object.entries(npmPackage._moduleAliases)) {
+  alias[item[0]] = path.join(rootDir, item[1]);
+}
 
 export default {
   mode: 'production',
   entry: path.join(rootDir, 'src/app.ts'),
   target: 'node',
   module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader'
-      }
-    ]
+    rules: [{
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+    }],
   },
   resolve: {
     extensions: [ '.tsx', '.ts', '.js' ],
-    alias: {
-      '@': path.join(rootDir, 'src')
-    }
+    alias,
   },
   output: {
     filename: 'app.js',
-    path: path.join(rootDir, 'dist')
+    path: path.join(rootDir, 'dist'),
   },
   plugins: [new (ESLintPlugin as any)()],
-  externals: [nodeExternals() as any]
+  externals: [nodeExternals()]
 } as webpack.Configuration;
