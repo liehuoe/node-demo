410f7d4b89922374dd58c1ce8ba3aee0500c24de
ckl <kanglie.chen@jumho.cn>
2021-01-12 11:32:26 +0800

    添加eslint规则

--- ts-app/.eslintrc.js
+++ ts-app/.eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   "rules": {
     "semi": [2, "always"],
-    // "global-require": 1,//使用import代替require
+    "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-unused-vars": "off",
     "@typescript-eslint/no-explicit-any": "off",
